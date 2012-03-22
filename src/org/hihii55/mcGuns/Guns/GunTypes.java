84fc08f3cc64b5509aa8df75a5d7d51058331bae
hihii55 <agsal2000@gmail.com>
2012-03-22 13:43:55 +0200

    Here are some more gun types.

--- src/org/hihii55/mcGuns/Guns/GunTypes.java
+++ src/org/hihii55/mcGuns/Guns/GunTypes.java
@@ -5,18 +5,43 @@ public enum GunTypes {
 	//First assault guns:
 	AK_47_Kalasnikov,
 	M16,
-	Heckler_Koch_Gewehr_36,
+	M46A1,
 	
 	//Then pistols:
 	Desert_Eagle,
+	G_18,
+	M1911
 	//More pistols coming soon...
 	
 	//Snipers:
 	
 	Barrett_M82,
 	Dragunov,
-	M21,
+	Intervention,
+
+	//Shotguns:
 	
+	SPAS_12,
+	STAKEOUT,
+	RANGER,
+
+	//Bazookas:
+
+	RPG_7,
+	M72_LAW,
+	APILAS,
+
+	//Light machine guns:
+
+	RPK,
+	RPD,
+	M240,
+
+	//Sub machine guns:
+
+	MP_5
+	UZI,
+	UMP_45
 	
 	NOT_GUN;
 	
